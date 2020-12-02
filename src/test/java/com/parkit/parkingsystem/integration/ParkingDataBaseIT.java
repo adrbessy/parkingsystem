7738612648113ea282976539d5fadba172e73bac
adrbessy <adrienbessy@hotmail.com>
2020-12-02 16:06:52 +0100

    create a test updateTicketTest

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -8,7 +8,6 @@ import static org.mockito.Mockito.when;
 
 import java.util.Date;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -52,47 +51,42 @@ public class ParkingDataBaseIT {
 		dataBasePrepareService.clearDataBaseEntries();
 	}
 
-	@AfterAll
-	private static void tearDown() {
-
+	@Test
+	@DisplayName("Verify that the next available spot for a CAR is spot 1 for empty DB")
+	public void getNextAvailableSpotCar_TEST() {
+		assertThat(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR)).isEqualTo(1);
 	}
 
 	@Test
-	public void testParkingACar() {
-		// ARRANGE
-		when(inputReaderUtil.readSelection()).thenReturn(1);
-		when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
-		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-		int nextAvailableSlot = parkingService.getNextParkingNumberIfAvailable().getId();
-
-		// ACT
-		parkingService.processIncomingVehicle();
-
-		// ASSERT
-		Ticket ticket = ticketDAO.getTicket("ABCDEF");
-		assertEquals(true, ticket != null);
-		assertEquals(nextAvailableSlot + 1, parkingService.getNextParkingNumberIfAvailable().getId());
-		assertEquals(false, ticket.getParkingSpot().isAvailable());
+	@DisplayName("Verify that the next available spot for BIKE is spot 4 for empty DB")
+	public void getNextAvailableSpotBike_TEST() {
+		assertThat(parkingSpotDAO.getNextAvailableSlot(ParkingType.BIKE)).isEqualTo(4);
 	}
 
 	@Test
-	public void testParkingLotExit() {
-		// ARRANGE
-		when(inputReaderUtil.readSelection()).thenReturn(1);
-		when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
-		testParkingACar();
-		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+	@DisplayName("Verify that when updating parking, will get the next available CAR spot")
+	public void updateParkingSpotCar_TEST() {
+		// GIVEN
+		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
 
-		// ACT
-		parkingService.processExitingVehicle();
-
-		// ASSERT
-		Ticket ticket = ticketDAO.getTicket("ABCDEF");
-		assertEquals(true, ticket.getPrice() >= 0);
-		assertEquals(true, ticket.getOutTime() != null);
+		// THEN
+		assertThat(parkingSpotDAO.updateParking(parkingSpot)).isTrue();
+		assertThat(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR)).isEqualTo(2);
 	}
 
 	@Test
+	@DisplayName("Verify that when updating parking, will get the next available BIKE spot")
+	public void updateParkingSpotBike_TEST() {
+		// GIVEN
+		ParkingSpot parkingSpot = new ParkingSpot(4, ParkingType.BIKE, false);
+
+		// THEN
+		assertThat(parkingSpotDAO.updateParking(parkingSpot)).isTrue();
+		assertThat(parkingSpotDAO.getNextAvailableSlot(ParkingType.BIKE)).isEqualTo(5);
+	}
+
+	@Test
+	@DisplayName("Verify that when the user gives a VehicleRegNumber already existing in the database, the user is considered as recurring")
 	void checkRecurringUserTest() {
 		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, true);
 		Ticket ticket = new Ticket();
@@ -111,6 +105,7 @@ public class ParkingDataBaseIT {
 	}
 
 	@Test
+	@DisplayName("Verify that when the user gives a VehicleRegNumber that doesn't exist in the database, the user is not considered as recurring")
 	void checkNotRecurringUserTest() {
 		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, true);
 		Ticket ticket = new Ticket();
@@ -129,42 +124,60 @@ public class ParkingDataBaseIT {
 	}
 
 	@Test
-	@DisplayName("Verify that the next available spot for a CAR is spot 1 for empty DB")
-	public void getNextAvailableSpotCar_TEST() {
-		parkingSpotDAO.dataBaseConfig = new DataBaseTestConfig();
-		assertThat(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR)).isEqualTo(1);
+	@DisplayName("Verify that when the user leaves the parking, the ticket is updated")
+	void updateTicketTest() {
+		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, true);
+		Ticket ticket = new Ticket();
+		Date inTime = new Date();
+		Date outTime = new Date();
+		ticket.setVehicleRegNumber("ABCDEF");
+		ticket.setParkingSpot(parkingSpot);
+		ticket.setId(1);
+		ticket.setPrice(0);
+		ticket.setInTime(inTime);
+		ticket.setOutTime(outTime);
+		ticketDAO.saveTicket(ticket);
+
+		boolean rep = ticketDAO.updateTicket(ticket);
+
+		assertTrue(rep);
 	}
 
 	@Test
-	@DisplayName("Verify that the next available spot for BIKE is spot 4 for empty DB")
-	public void getNextAvailableSpotBike_TEST() {
-		parkingSpotDAO.dataBaseConfig = new DataBaseTestConfig();
-		assertThat(parkingSpotDAO.getNextAvailableSlot(ParkingType.BIKE)).isEqualTo(4);
+	@DisplayName("Verify that when the user comes into the parking, the ticket is saved correctly")
+	public void testParkingACar() {
+		// ARRANGE
+		when(inputReaderUtil.readSelection()).thenReturn(1);
+		when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+		int nextAvailableSlot = parkingService.getNextParkingNumberIfAvailable().getId();
+
+		// ACT
+		parkingService.processIncomingVehicle();
+
+		// ASSERT
+		Ticket ticket = ticketDAO.getTicket("ABCDEF");
+		assertEquals(true, ticket != null);
+		assertEquals(nextAvailableSlot + 1, parkingService.getNextParkingNumberIfAvailable().getId());
+		assertEquals(false, ticket.getParkingSpot().isAvailable());
 	}
 
 	@Test
-	@DisplayName("Verify that when updating parking, will get the next available CAR spot")
-	public void updateParkingSpotCar_TEST() {
-		// GIVEN
-		parkingSpotDAO.dataBaseConfig = new DataBaseTestConfig();
-		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+	@DisplayName("Verify that when the user leaves the parking, the price is not null and the exit time is not null")
+	public void testExitAcar() {
+		// ARRANGE
+		when(inputReaderUtil.readSelection()).thenReturn(1);
+		when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+		testParkingACar();
+		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
 
-		// THEN
-		assertThat(parkingSpotDAO.updateParking(parkingSpot)).isTrue();
-		assertThat(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR)).isEqualTo(2);
-	}
+		// ACT
+		parkingService.processExitingVehicle();
 
-	@Test
-	@DisplayName("Verify that when updating parking, will get the next available BIKE spot")
-	public void updateParkingSpotBike_TEST() {
-		// GIVEN
-		parkingSpotDAO.dataBaseConfig = new DataBaseTestConfig();
-		ParkingSpot parkingSpot = new ParkingSpot(4, ParkingType.BIKE, false);
-
-		// THEN
-		assertThat(parkingSpotDAO.updateParking(parkingSpot)).isTrue();
-		assertThat(parkingSpotDAO.getNextAvailableSlot(ParkingType.BIKE)).isEqualTo(5);
-
+		// ASSERT
+		Ticket ticket = ticketDAO.getTicket("ABCDEF");
+		assertEquals(true, ticket.getPrice() >= 0);
+		assertEquals(true, ticket.getOutTime() != null);
 	}
 
 }
